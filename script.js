b7f56dab0bc444e57934766167e3acc9a0bbb59b
LastCleanShirt <darealfoxcat123@gmail.com>
2023-06-19 17:26:30 +0700

    changes for the text

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 // HTML element
 //const heading = document.querySelector('');
-const typingSpeed = 100;
+const typingSpeed = 25;
 const expand = $(".expand")
+
+let heading = $("#page-1 h1.normal")
+
+const dialogues = [
+"I made this website specifically for ya :)",
+"But i still dont know what to put in here :(",
+  ]
+
+let currentDialogue = 0
+let onDialogue = false
 
 const ad1_pg1 = $("#page-1 .expand")
 
 ad1_pg1.on("click", () => {
-  clearText("I made this website specifically for you :)", "#page-1 h1.normal")
+  if (!onDialogue)
+  {
+    onDialogue = true
+    console.log("You pressed but without dialogu")
+    if (currentDialogue < dialogues.length) {
+      clearText(dialogues[currentDialogue], "#page-1 h1.normal")
+      currentDialogue++;
+    } else {
+
+      // TODO: First dialogues were done
+      expand.css({"animation": "0.5s slideOut ease-in-out"})
+      expand.on("animationend", () => { expand.remove() })
+      $(heading).css({"animation": "0.5s slideOut ease-in-out"})
+      $(heading).on("animationend", () => { $(heading).remove() })
+
+      $("#page-1").css({"animation": "1s changeBg ease-in-out"})
+      $("#page-1").css({"background-color": "#B3C890"})
+
+    } 
+  } else {
+    console.log("You pressed but dialogue is still here bitch")
+  }
 })
 
 function clearText(t,q) {
   const heading = $(q);
   //expand.style.animation = "1s slideOut ease-in-out";
-  expand.css({"animation": "1s slideOut ease-in-out"})
-  expand.on("animationend", () => {
-    expand.remove()
-  })
+
   let i = heading.text().length;
   const timer = setInterval(() => {
+    console.log(onDialogue)
     heading.text(heading.text().slice(0, -1));
     i--;
 
     if (i === 0) {
       clearInterval(timer);
       typeEffect(t, heading);
+
     }
   }, typingSpeed);
+
+
 
 }
 
@@ -37,6 +69,8 @@ function typeEffect(textToType, heading) {
 
     if (i === textToType.length) {
       clearInterval(timer);
+      onDialogue = false
+      console.log("Its doone")
     }
   }, typingSpeed);
 }
